9e9c08ddb4f218f93081e3d0bd04827bee6e1c85
gundapureddysaiteja <116089043+gundapureddysaiteja@users.noreply.github.com>
2022-12-10 15:35:07 +0530

    Add files via upload

---
+++ lib/theme_config.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightTheme = ThemeData.light().copyWith(
+  appBarTheme: AppBarTheme(
+    backgroundColor: Colors.green[700],
+  )
+);
+
+ThemeData darkTheme = ThemeData.dark();
